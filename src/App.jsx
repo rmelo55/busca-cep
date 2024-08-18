63fab5f5250663712e8be5822db9db1a6bf7c0f0
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-08-18 18:58:08 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+
+import { FiSearch } from 'react-icons/fi'
+import './App.css'
+
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
 
-      <h1 className="title">Busca CEP</h1>
+      <h1 className='title'>Busca CEP</h1>
 
-      <div className="containerInput">
+      <div className='containerInput'>
         <input 
-        type="text" 
-        placeholder="Digite seu CEP..." 
+        type='text' 
+        placeholder='Digite seu CEP...' 
         />
 
-        <button className="buttonSearch">
-          Procurar
+        <button className='buttonSearch'>
+          <FiSearch size={25} color={'#fff'} />
         </button>
       </div>
 
+      <main className='main'>
+        <h2>CEP: 24904100</h2>
+        <span>Complemento: Algum Complemento</span>
+        <span>Flamengo</span>
+        <span>Maricá - RJ</span>
+      </main>
 
     </div>
   );
